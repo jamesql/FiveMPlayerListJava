6120a3467de7934de74d89efa2d54ab8bb2a05bc
James <42484571+smitteh1@users.noreply.github.com>
2018-10-27 12:34:47 -0400

    removed things not needed for personal use

--- source/config.java
+++ source/config.java
@@ -1,17 +1,4 @@
 
 public class config {
-
 	final static String SERVER_IP = "95.216.104.84:30120";
-	String username = "james";
-	public String password = "9b69b4e5842f6706d44d4e8175ccff839730cf7497869ac4830ce66c0d7b98d7";
-	private final String PIN_NUM = "secretword";
-	public String errMsg = "No Perms!";
-	
-	
-	public String getPassword(String pin) {
-		if (pin == PIN_NUM)
-		return password;
-		else
-		return errMsg;
-	}
 }
